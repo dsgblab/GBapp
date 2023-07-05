200647b00a22d91f166d4799d0892d81496e0e90
Daniel Correa Hincapié <54317215+Ryunosukee@users.noreply.github.com>
2023-07-05 10:53:23 -0500

    documentation

--- config/power-bi.php
+++ config/power-bi.php
@@ -2,8 +2,8 @@
 
 return [
     'user_id' => env('POWERBI_USER_ID'),
-    'grant_type' => env('POWERBI_GRANT_TYPE'),
+    'grant_type' => env('POWERBI_GRANT_TYPE', 'client_credentials'),
     'client_secret' => env('POWERBI_CLIENT_SECRET'),
     'client_id' => env('POWERBI_CLIENT_ID'),
-    'resource' => env('POWERBI_RESOURCE'),
+    'resource' => env('POWERBI_RESOURCE', 'https://analysis.windows.net/powerbi/api'),
 ];
