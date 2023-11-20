5d217c5db583e1fb14da16e1ea577bc116ca7374
Daniel Hincapié <54317215+Ryunosukee@users.noreply.github.com>
2023-11-20 11:28:48 -0500

    Update PowerBIAccessToken.php

--- app/Http/Middleware/PowerBIAccessToken.php
+++ app/Http/Middleware/PowerBIAccessToken.php
@@ -16,10 +16,9 @@ class PowerBIAccessToken
     use PowerBITrait;
 
     /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     *
+     * @param Request $request
+     * @param Closure $next
+     * @return Response
      * @throws GuzzleException
      */
     public function handle(Request $request, Closure $next): Response
